aa9d4a45330b33e0c9a1437aeff53b9a37aadba8
Kelvin Ferdinan <91706611+mispipin@users.noreply.github.com>
2025-02-24 12:12:52 +0700

    Add files via upload

---
+++ 02_Pengenalan_IDE_dan_Pemrograman_Node.JS/variable-local-global.js
@@ -0,0 +1,12 @@
+let globalVariable = "Kelvin Ferdinan"
+
+function getMyIdentity(){
+    let localVariable = 2311104009
+
+    console.log(`Nama saya ${globalVariable} nim ${localVariable}`)
+}
+
+console.log(globalVariable)
+console.log(localVariable)
+
+getMyIdentity()
---
+++ 02_Pengenalan_IDE_dan_Pemrograman_Node.JS/variable.js
@@ -0,0 +1,17 @@
+var name = "Kevin Jonson"
+const nim = 2311104021
+
+console.log(name)
+console.log(nim)
+
+console.log("nama saya adalah " + name + " dan nim saya adalah " + nim)
+console.log(`Nama saya adalah ${name} dan nim saya adalah ${nim}`)
+
+console.log(`Nama saya adalah ${name} 
+    nim saya adalah ${nim}`)
+
+   /* var name = "ayam"
+
+    let fullName = "Kevin Jonson"
+
+    nim = 2311104021*/
